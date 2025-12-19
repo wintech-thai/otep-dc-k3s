6f269a38b990684fea29a4e144560747e4e0a579
pjames <pjames@gmail.com>
2025-12-19 14:21:28 +0700

    Added Prometheus CRD

--- 03-boot-strap.bash
+++ 03-boot-strap.bash
@@ -6,6 +6,14 @@ cd 01-bootstrap
 
 kubectl apply -f argocd-app.yaml
 kubectl apply -f argocd-ing.yaml
-kubectl apply -f app-monitoring-crd.yaml
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install kube-prometheus-crds \
+  prometheus-community/kube-prometheus-stack \
+  --version 80.5.0 \
+  --namespace monitoring \
+  --create-namespace \
+  --include-crds
 
 cd ..
