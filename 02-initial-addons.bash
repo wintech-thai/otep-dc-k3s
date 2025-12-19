08b4f9b0989c334e971f2ceccadfd595b5108f9e
pjames <pjames@gmail.com>
2025-12-19 11:18:44 +0700

    Added ingress nginx

---
+++ 02-initial-addons.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export $(xargs <.env)
+
+DST_DIR=addons
+
+# ต้องเพิ่ม volume map ตรง k3s volume ด้วย
+SRC_FILE=00-configs/addons-argocd.yaml
+echo "Copying file [${SRC_FILE}] to directory [${DST_DIR}]"
+cp ${SRC_FILE} ${DST_DIR}
+
+SRC_FILE=00-configs/addons-ingress.yaml
+echo "Copying file [${SRC_FILE}] to directory [${DST_DIR}]"
+cp ${SRC_FILE} ${DST_FILE}
