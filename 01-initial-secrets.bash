5119f4f4cfa17ccfb9c1519daaf57822755faf7b
pjames <pjames@gmail.com>
2025-12-19 10:32:57 +0700

    Added 01-initial-secrets.bash

---
+++ 01-initial-secrets.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SRC_FILE=.env
+DST_FILE=addons/initial-secrets.yaml
+SECRET=initial-secret
+TMP_FILE=/tmp/${SECRET}.tmp
+
+cat <<END > "${TMP_FILE}"
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ${SECRET}
+type: Opaque
+data:
+END
+
+cat ${SRC_FILE} | while read line
+do
+  regex="^(.+?)=(.+)$"
+  if [[ $line =~ $regex ]]; then
+
+    KEY=$(echo -e "$line" | perl -0777 -ne 'print $1 if /^(.+?)=(.+)$/')
+    VALUE=$(echo -e "$line" | perl -0777 -ne 'print $2 if /^(.+?)=(.+)$/')
+
+    echo "  ${KEY}: $(echo -n "${VALUE}" | base64 -w0)" >> ${TMP_FILE}
+  fi
+done
+
+export $(xargs <.env)
+
+cp ${TMP_FILE} ${DST_FILE}
